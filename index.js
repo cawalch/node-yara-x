a3d80247b6a46f3e2dd83a5630f68263c7aab9f5
Caleb Walch <cawalch@gmail.com>
2025-03-30 10:22:07 -0500

    chore: rename package to @litko/yara-x
    Rename package to scoped npm package @litko/yara-x.
    
    <!-- ps-id: 411fd68f-6856-4217-910a-e009d47fd8f8 -->

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./yara-x.android-arm64.node')
           } else {
-            nativeBinding = require('yara-x-android-arm64')
+            nativeBinding = require('@litko/yara-x-android-arm64')
           }
         } catch (e) {
           loadError = e
@@ -49,7 +49,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./yara-x.android-arm-eabi.node')
           } else {
-            nativeBinding = require('yara-x-android-arm-eabi')
+            nativeBinding = require('@litko/yara-x-android-arm-eabi')
           }
         } catch (e) {
           loadError = e
@@ -69,7 +69,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./yara-x.win32-x64-msvc.node')
           } else {
-            nativeBinding = require('yara-x-win32-x64-msvc')
+            nativeBinding = require('@litko/yara-x-win32-x64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -83,7 +83,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./yara-x.win32-ia32-msvc.node')
           } else {
-            nativeBinding = require('yara-x-win32-ia32-msvc')
+            nativeBinding = require('@litko/yara-x-win32-ia32-msvc')
           }
         } catch (e) {
           loadError = e
@@ -97,7 +97,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./yara-x.win32-arm64-msvc.node')
           } else {
-            nativeBinding = require('yara-x-win32-arm64-msvc')
+            nativeBinding = require('@litko/yara-x-win32-arm64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -113,7 +113,7 @@ switch (platform) {
       if (localFileExisted) {
         nativeBinding = require('./yara-x.darwin-universal.node')
       } else {
-        nativeBinding = require('yara-x-darwin-universal')
+        nativeBinding = require('@litko/yara-x-darwin-universal')
       }
       break
     } catch {}
@@ -124,7 +124,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./yara-x.darwin-x64.node')
           } else {
-            nativeBinding = require('yara-x-darwin-x64')
+            nativeBinding = require('@litko/yara-x-darwin-x64')
           }
         } catch (e) {
           loadError = e
@@ -138,7 +138,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./yara-x.darwin-arm64.node')
           } else {
-            nativeBinding = require('yara-x-darwin-arm64')
+            nativeBinding = require('@litko/yara-x-darwin-arm64')
           }
         } catch (e) {
           loadError = e
@@ -157,7 +157,7 @@ switch (platform) {
       if (localFileExisted) {
         nativeBinding = require('./yara-x.freebsd-x64.node')
       } else {
-        nativeBinding = require('yara-x-freebsd-x64')
+        nativeBinding = require('@litko/yara-x-freebsd-x64')
       }
     } catch (e) {
       loadError = e
@@ -174,7 +174,7 @@ switch (platform) {
             if (localFileExisted) {
               nativeBinding = require('./yara-x.linux-x64-musl.node')
             } else {
-              nativeBinding = require('yara-x-linux-x64-musl')
+              nativeBinding = require('@litko/yara-x-linux-x64-musl')
             }
           } catch (e) {
             loadError = e
@@ -187,7 +187,7 @@ switch (platform) {
             if (localFileExisted) {
               nativeBinding = require('./yara-x.linux-x64-gnu.node')
             } else {
-              nativeBinding = require('yara-x-linux-x64-gnu')
+              nativeBinding = require('@litko/yara-x-linux-x64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -203,7 +203,7 @@ switch (platform) {
             if (localFileExisted) {
               nativeBinding = require('./yara-x.linux-arm64-musl.node')
             } else {
-              nativeBinding = require('yara-x-linux-arm64-musl')
+              nativeBinding = require('@litko/yara-x-linux-arm64-musl')
             }
           } catch (e) {
             loadError = e
@@ -216,7 +216,7 @@ switch (platform) {
             if (localFileExisted) {
               nativeBinding = require('./yara-x.linux-arm64-gnu.node')
             } else {
-              nativeBinding = require('yara-x-linux-arm64-gnu')
+              nativeBinding = require('@litko/yara-x-linux-arm64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -232,7 +232,7 @@ switch (platform) {
             if (localFileExisted) {
               nativeBinding = require('./yara-x.linux-arm-musleabihf.node')
             } else {
-              nativeBinding = require('yara-x-linux-arm-musleabihf')
+              nativeBinding = require('@litko/yara-x-linux-arm-musleabihf')
             }
           } catch (e) {
             loadError = e
@@ -245,7 +245,7 @@ switch (platform) {
             if (localFileExisted) {
               nativeBinding = require('./yara-x.linux-arm-gnueabihf.node')
             } else {
-              nativeBinding = require('yara-x-linux-arm-gnueabihf')
+              nativeBinding = require('@litko/yara-x-linux-arm-gnueabihf')
             }
           } catch (e) {
             loadError = e
@@ -261,7 +261,7 @@ switch (platform) {
             if (localFileExisted) {
               nativeBinding = require('./yara-x.linux-riscv64-musl.node')
             } else {
-              nativeBinding = require('yara-x-linux-riscv64-musl')
+              nativeBinding = require('@litko/yara-x-linux-riscv64-musl')
             }
           } catch (e) {
             loadError = e
@@ -274,7 +274,7 @@ switch (platform) {
             if (localFileExisted) {
               nativeBinding = require('./yara-x.linux-riscv64-gnu.node')
             } else {
-              nativeBinding = require('yara-x-linux-riscv64-gnu')
+              nativeBinding = require('@litko/yara-x-linux-riscv64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -289,7 +289,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./yara-x.linux-s390x-gnu.node')
           } else {
-            nativeBinding = require('yara-x-linux-s390x-gnu')
+            nativeBinding = require('@litko/yara-x-linux-s390x-gnu')
           }
         } catch (e) {
           loadError = e
